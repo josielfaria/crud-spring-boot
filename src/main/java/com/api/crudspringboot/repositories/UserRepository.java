fd6bc279fdca4f933de9fa6802ba714d519ac221
Josiel Faria <developerjosiel@gmail.com>
2022-04-13 23:45:46 -0300

    Adicionando projeto com crud basico de usuario

---
+++ src/main/java/com/api/crudspringboot/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.api.crudspringboot.repositories;
+
+import com.api.crudspringboot.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
+
+    boolean existsByUsername(String username);
+
+}
---
+++ src/main/java/com/api/crudspringboot/services/UserService.java
@@ -0,0 +1,42 @@
+package com.api.crudspringboot.services;
+
+import com.api.crudspringboot.models.UserModel;
+import com.api.crudspringboot.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class UserService {
+
+    final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Transactional
+    public UserModel save(UserModel userModel) {
+        return userRepository.save(userModel);
+    }
+
+    public boolean existUsername(String username) {
+        return userRepository.existsByUsername(username);
+    }
+
+    public List<UserModel> findAll() {
+        return userRepository.findAll();
+    }
+
+    public Optional<UserModel> findById(UUID id) {
+        return userRepository.findById(id);
+    }
+
+    @Transactional
+    public void delete(UserModel userModel) {
+        userRepository.delete(userModel);
+    }
+}
